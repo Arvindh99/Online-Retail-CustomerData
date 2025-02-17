08109c835cd01dea8f7923019ed715eb54ed6535
Arvindh <cvarvindh@gmail.com>
2025-02-17 19:11:47 +0530

    WebApp.py

--- WebApp.py
+++ WebApp.py
@@ -265,9 +265,8 @@ if page == "📊 RFM Analysis":
         # Chart 1: Top 10 Products by Total Sales
         if i == 0:
             segment_counts = rfm['Segment'].value_counts().reset_index()
-            fig1 = px.bar(segment_counts, x='count', y='Segment', color='index', 
-                 title='Customer Segment Distribution', 
-                 labels={'index': 'Segment', 'Segment': 'Count'},
+            fig1 = px.bar(segment_counts, x='Segment', y='count', color='index', 
+                 title='Customer Segment Distribution',
                  color_discrete_sequence=px.colors.qualitative.Pastel)
             col1.plotly_chart(fig1)
             
